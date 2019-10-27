cd57275e0bc77ee47270b3bb4557a7909a75a001
dkamyshov <danil.kamyshov@gmail.com>
2019-10-27 22:36:43 +0300

    step 5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import mireaLogoImage from "./mirea.png";
 
 mireaLogo.src = mireaLogoImage;
+
+const counterElement = document.getElementById("counter-value");
+const counterIncButton = document.getElementById("counter-inc");
+const counterDecButton = document.getElementById("counter-dec");
+
+/**
+ * Состояние приложения (в данном случае - значение счетчика).
+ */
+let counter = 0;
+
+counterIncButton.addEventListener("click", () => {
+  counter++;
+  counterElement.innerHTML = String(counter);
+});
+
+counterDecButton.addEventListener("click", () => {
+  counter--;
+  counterElement.innerHTML = String(counter);
+});
